2af1bd845a422d9939cafce33938216d3593b0e3
Daniel Ström <D@nielstrom.se>
2014-05-28 20:45:23 +0200

    Add snap-svg to start working on the robots.

---
+++ public/arena.js
@@ -0,0 +1,10 @@
+//var s = Snap("#svg");
+var arena = (function () {
+	function print_something() {
+		console.log("Arena");
+	}
+
+	return {
+		'print_something': print_something
+	}
+}());
--- router.js
+++ router.js
@@ -11,7 +11,15 @@ Router.map( function () {
   this.route('arenas_show', {
     path: '/arenas/:_id',
     waitOn: function() { return Meteor.subscribe('arenas', this.params._id)},
-    data: function () { return Arenas.findOne(this.params._id) }
+    data: function () { return Arenas.findOne(this.params._id) },
+    action: function() {
+      var self = this;
+      $.getScript('/arena.js', function(data, textStatus, jqxhr) {
+        if(jqxhr.status === 200) {
+          self.render();
+        }
+     });
+    }
   });
 
   this.route('about');
